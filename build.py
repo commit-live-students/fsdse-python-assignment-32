08bc9650c05aaa74b5b7f3a9ced988dcfb4e2767
rishabh-github <rishabh-github@users.noreply.github.com>
2017-05-29 17:29:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
+    sum_numbers = 0
+    for n in list_of_integers:
+        sum_numbers = sum_numbers+n
+
     return sum_numbers
