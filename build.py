f980bfe798ccb019862cf1b641bbb834f2676f42
seekingpeace <seekingpeace@users.noreply.github.com>
2017-06-03 08:00:05 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
 def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
+    sum_numbers=sum(list_of_integers)
+    print(sum_numbers)
     return sum_numbers
+solution([1,2,3])
