a81cda3d0ed294bc06907a711792d71138d4eb8f
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-12 20:36:00 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
-def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
-    return sum_numbers
+def solution(list_of_ints):
+    sum_no = 0
+    for k in range(0,len(list_of_ints)):
+        sum_no = sum_no + list_of_ints[k]
+    return sum_no
+
+print solution([5,4,6])
