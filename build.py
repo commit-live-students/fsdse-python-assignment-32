ce12adbc0cef6bf72380eeef81bf6edd429bede7
varunpanicker <varunpanicker@users.noreply.github.com>
2017-06-17 01:06:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,11 @@
+list_of_integers=[1,2,-8]
 def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
+#    def sum_list(items):
+    sum_numbers = 0
+    for x in list_of_integers:
+        sum_numbers += x
     return sum_numbers
+    print sum_numbers
+#print(sum_list([1,2,-8]))
+
+solution (list_of_integers)
