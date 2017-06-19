9d7c67f7a8a35cf6980a8a163b843c35b8152701
Bicky23 <bijitdeka23@gmail.com>
2017-06-19 17:24:15 +0000

    Done

--- build.py
+++ build.py
@@ -2,5 +2,9 @@ def solution(list_of_integers):
     '''
     Enter your code here
     '''
-    
+    sum_numbers = 0
+    for i in list_of_integers:
+        sum_numbers = sum_numbers + i
+
+
     return sum_numbers
