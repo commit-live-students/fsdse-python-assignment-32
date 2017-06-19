f1234ee797403a17162dc55f620ebc647422f1d0
adinathauti <adinathauti@gmail.com>
2017-06-19 09:01:56 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
-def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
-    return sum_numbers
+def solution(lst):
+    s = 0
+    for e in lst:
+        s = s+e
+    return s
+
+t=solution([1,2,3,4])
+print t
