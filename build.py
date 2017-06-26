f78e21eb7eb5c99102575b49d3d322f36da5e1f8
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-26 15:15:41 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
+    sum_numbers = 0
+    for x in list_of_integers:
+        sum_numbers += x
     return sum_numbers
+
+solution([2,3,5,8,11])
