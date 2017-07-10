194bd2254ccd3eb500397c90a48e4388d7e5ca20
PankajMehar <pankaj02mehar@gmail.com>
2017-07-10 07:16:13 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 def solution(list_of_integers):
-    '''
-    Enter your code here
-    '''
-    
-    return sum_numbers
+    summ = 0
+    for x in list_of_integers:
+        summ += x
+    return summ
+print(solution([2,3,5,8,11]))
